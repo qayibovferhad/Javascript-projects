4470ddaf4cd241c826dafbee746e153032550629
Qayibov Ferhad <64160419+qayibovferhad@users.noreply.github.com>
2023-02-08 13:45:27 +0400

    Add files via upload

---
+++ Country Guide/script.js
@@ -0,0 +1,61 @@
+const countryInput = document.querySelector("#country-inp");
+const searchBtn = document.querySelector("#search-btn");
+const result = document.querySelector("#result");
+searchBtn.addEventListener("click", function () {
+  fetchCountry();
+});
+function fetchCountry() {
+  const countryName = countryInput.value;
+  fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
+    .then((response) => response.json())
+    .then((data) => countryGuide(data))
+    .catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+      } else {
+        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+      }
+    });
+}
+
+function countryGuide(data) {
+  result.innerHTML = `
+        <img src="${data[0].flags.svg}" class="flag-img">
+        <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data[0].capital[0]}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Continent:</h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population:</h4>
+                <span>${data[0].population}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Currency:</h4>
+                <span>${
+                  data[0].currencies[Object.keys(data[0].currencies)].name
+                } - ${Object.keys(data[0].currencies)[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Common Languages:</h4>
+                <span>${Object.values(data[0].languages)
+                  .toString()
+                  .split(",")
+                  .join(", ")}</span>
+            </div>
+        </div>
+      `;
+}
