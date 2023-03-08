ab75397b04245a87d804413ce9b3efacdc2e9d13
Qayibov Ferhad <64160419+qayibovferhad@users.noreply.github.com>
2023-03-08 23:26:25 +0400

    Add files via upload

---
+++ Dog Makeover Game/script.js
@@ -0,0 +1,41 @@
+let currentElement = "";
+let initialX = 0,
+  initialY = 0;
+
+let isTouchDevice = () => {
+  try {
+    document.createEvent("TouchEvent");
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+function dragStart(e) {
+  initialX = isTouchDevice() ? e.touches[0].clientX : e.clientX;
+  initialY = isTouchDevice() ? e.touches[0].clientY : e.clientY;
+  currentElement = e.target;
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+
+const drop = (e) => {
+  e.preventDefault();
+  let newX = isTouchDevice() ? e.touches[0].clientX : e.clientX;
+  let newY = isTouchDevice() ? e.touches[0].clientY : e.clientY;
+  currentElement.style.top =
+    currentElement.offsetTop - (initialY - newY) + "px";
+  currentElement.style.left =
+    currentElement.offsetLeft - (initialX - newX) + "px";
+};
+
+window.onload = () => {
+  currentElement = "";
+  let body = document.body;
+  body.addEventListener("dragstart", dragStart, false);
+  body.addEventListener("dragover", dragOver, false);
+  body.addEventListener("drop", drop, false);
+  body.addEventListener("touchstart", dragStart, false);
+  body.addEventListener("touchmove", drop, false);
+};
